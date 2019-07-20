a15f1df306e48d332531f204fc015823e29c61e1
adamprobert <adamprobert@live.co.uk>
2019-07-20 13:38:36 +0100

    Added basic map to start

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import Phaser from "phaser";
 import BootScene from "./scenes/Boot";
+import GameScene from "./scenes/Game";
 
 var config = {
   type: Phaser.AUTO,
   parent: "content",
   width: 800,
   height: 700,
-  scene: [BootScene]
+  zoom: 3,
+  scene: [GameScene]
 };
 
 window.game = new Phaser.Game(config);
