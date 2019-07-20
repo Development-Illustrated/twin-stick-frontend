a767b656aa2cb628dfbf1acbf2269f0b30bc8c2b
adamprobert <adamprobert@live.co.uk>
2019-07-20 14:38:14 +0100

    Added new dope ass map

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -2,14 +2,15 @@ import Phaser from "phaser";
 
 class GameScene extends Phaser.Scene {
     preload() {
-        this.load.image("groundTiles", "../src/assets/tilesets/galletcity_tiles.png");
-        this.load.tilemapTiledJSON('crossroads', '../src/assets/tilesets/tilemap.json');
+        this.load.image("groundTiles", "../src/assets/tilesets/TileA5_PHC_Exterior-General.png");
+        this.load.tilemapTiledJSON('crossroads', '../src/assets/tilesets/horrormap.json');
     }
     create() {
         const map = this.make.tilemap({ key: 'crossroads' });
-        const groundTiles = map.addTilesetImage('TestTileset', 'groundTiles');
-
-        map.createDynamicLayer('Tile Layer 1', [groundTiles]);
+        const groundTiles = map.addTilesetImage('horrorset', 'groundTiles');
+        this.bgLayer = map.createDynamicLayer('Tile Layer 1', [groundTiles]);
+        this.wallsLayer = map.createDynamicLayer('Tile Layer 2', [groundTiles]);
+        map.setCollisionByExclusion([0], true, this.wallsLayer);
 
     }
 }
